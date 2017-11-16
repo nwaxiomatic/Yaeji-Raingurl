6292500c5dea128d937c28ef9a4bac7940de57cb
mac <nicholasdwoodward@gmail.com>
2017-11-16 13:18:45 -0500

    hide address bar

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,20 @@
-// https://developers.google.com/youtube/iframe_api_reference
+window.addEventListener("load",function() {
+  setTimeout(function(){
+    window.scrollTo(0, 1);
+  }, 0);
+});
 
-// global variable for the player
 var player;
 
-// this function gets called when API is ready to use
 function onYouTubePlayerAPIReady() {
-  // create the global player from the specific iframe (#video)
   player = new YT.Player('video', {
 	events: {
-	  // call this function when player is ready to use
 	  'onReady': onPlayerReady
 	}
   });
 }
 
 function onPlayerReady(event) {
-  
-  // bind events
   $('.loader').toggleClass('shown');
   $('.buttons').toggleClass('shown');
 
@@ -28,7 +26,6 @@ function onPlayerReady(event) {
   
 }
 
-// Inject YouTube API script
 var tag = document.createElement('script');
 tag.src = "//www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
